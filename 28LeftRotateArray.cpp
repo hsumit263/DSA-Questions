28e1e592ebd85c06066a1c08af841a379e9486ea
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-09-10 20:53:21 +0530

    Update 28LeftRotateArray.cpp

--- 28LeftRotateArray.cpp
+++ 28LeftRotateArray.cpp
@@ -11,3 +11,25 @@ vector<int> ans(arr.size());
 
     return ans;
 
+// Second Approach
+ vector<int> ans;
+
+    int n=arr.size();
+    k=k%n;
+
+    // first putting k elements in ans array
+    for(int i=0;i<k;i++){
+        ans.push_back(arr[i]);
+    }
+
+    // then shifting the remaining elements in the array by n-k places
+    for(int i=k;i<n;i++){
+        arr[i-k]=arr[i];
+    }
+
+    // copying the elements from ans array 
+    for(int i=n-k;i<n;i++){
+        arr[i]=ans[i-(n-k)];
+    }
+
+    return arr;

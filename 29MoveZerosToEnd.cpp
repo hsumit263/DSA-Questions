25b2e9642ab1f8dc274e7db4b49466767ff42678
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-09-11 23:17:12 +0530

    Update 29MoveZerosToEnd.cpp

--- 29MoveZerosToEnd.cpp
+++ 29MoveZerosToEnd.cpp
@@ -20,6 +20,26 @@ vector<int> moveZeros(int n, vector<int> a) {
 
     return ans;
 
-    
+    // Optimal approach
+    int j=-1;
+    for(int i=0;i<n;i++){
+        if(a[i]==0){
+            j=i;
+            break;
+        }
+
+    }
+
+    if(j==-1) return a;
+
+    for(int i=j+1;i<n;i++){
+        if(a[i]!=0){
+            swap(a[i],a[j]);
+            j++;
+        }
+
+    }
+
+    return a;
 
 }

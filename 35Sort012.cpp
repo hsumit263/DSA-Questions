df0eec647c14e85f2890a6c9309777fc41c7042f
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-09-16 18:32:52 +0530

    Create 35Sort012.cpp

---
+++ 35Sort012.cpp
@@ -0,0 +1,47 @@
+// Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+ void sort012(int a[], int n)
+    {
+        //First approach 
+        // int low=0;
+        // int mid=0;
+        // int high=n-1;
+        
+        // while(mid<=high){
+        //     if(a[mid]==0) swap(a[low++],a[mid++]);
+            
+        //     else if(a[mid]==1) mid++;
+            
+        //     else swap(a[mid],a[high--]);
+                
+            
+        // }
+        
+        // return;
+
+        // Second approach
+           int zero=0;
+        int one=0;
+        int two=0;
+        
+        for(int i=0;i<n;i++){
+            if(a[i]==0) zero++;
+            else if(a[i]==1) one++;
+            else two++;
+        }
+        
+        int i=0;
+        while(zero--){
+            a[i++]=0;
+        }
+        
+        while(one--){
+            a[i++]=1;
+        }
+        
+        while(two--){
+            a[i++]=2;
+        }
+        
+
+    }

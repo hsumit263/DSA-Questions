94a24f09d55a22f83f8eca3d8b3cc2d3ad22366d
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-09-18 21:07:59 +0530

    Update 36LongestSubarrayWithSumK.cpp

--- 36LongestSubarrayWithSumK.cpp
+++ 36LongestSubarrayWithSumK.cpp
@@ -1,5 +1,7 @@
 // You are given an array 'a' of size 'n' and an integer 'k'. Find the length of the longest subarray
-// of 'a' whose sum is equal to 'k'
+// of 'a' whose sum is equal to 'k' where k is positive
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
 
 // Brute force approach
     // int maxSize=0;
@@ -15,3 +17,33 @@
     // }
 
     // return maxSize;
+
+    // Optimal approach
+    int left=0;
+    int right=0;
+    int maxLen=0;
+    int n=a.size();
+
+    long long sum=a[0];
+
+    while(right<n){
+        while(left<=right && sum>k){
+            sum-=a[left];
+            left++;
+        }
+
+        if(sum==k) {
+            maxLen=max(maxLen,right-left+1);
+        }
+
+        right++;
+        if(right<n){
+           
+            sum+=a[right];
+        }
+
+    }
+
+    return maxLen;
+    
+}

973e3f1685540d1654f7cdceac9127babfb299b0
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-09-19 22:17:41 +0530

    Create 37TwoSum.cpp

---
+++ 37TwoSum.cpp
@@ -0,0 +1,15 @@
+// Sam wants to read exactly 'target' number of pages . He has an array 'book' containing number of pages 
+// for "N" books . Return YES?NO, if it is possible  for him to read any 2 books and he can meet his 
+// 'target' number of pages.
+
+string read(int n, vector<int> book, int target)
+{   
+    // brute force approach
+    // for(int i=0;i<n;i++){
+    //     for(int j=1;j<n;j++){
+    //         if(book[i]+book[j]==target) return "YES";
+    //     }
+    // }
+
+    // return "NO";
+}

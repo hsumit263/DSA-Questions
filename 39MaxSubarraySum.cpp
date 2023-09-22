bbc73b866b45dbf3dce6d9e3156c20cdcc2e4082
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-09-22 23:48:25 +0530

    Create 39MaxSubarraySum.cpp

---
+++ 39MaxSubarraySum.cpp
@@ -0,0 +1,34 @@
+long long maxSubarraySum(vector<int> arr, int n)
+{   
+   // brute force approach
+//    long long maxi=INT_MIN;
+
+//    for(int i=0;i<n;i++){
+       
+//        long long sum=0;
+
+//        for(int j=i;j<n;j++){
+
+//            sum+=arr[j];
+
+//            maxi=max(sum,maxi);
+//        }
+//    }
+
+//    return maxi;
+
+    long long sum=0;
+    long long maxi=LONG_MIN;
+
+    for(int i=0;i<n;i++){
+
+        sum+=arr[i];
+
+        if(sum>maxi) maxi=sum;
+
+        if(sum<0) sum=0;
+    }
+
+    if(maxi<0) return 0;
+    else return maxi;
+}

f4536cf08b7466c5569cf3b0aeec8028e3786d14
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-09-28 22:57:53 +0530

    Update 44SetMatrixZero.cpp

--- 44SetMatrixZero.cpp
+++ 44SetMatrixZero.cpp
@@ -29,5 +29,58 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
 
 	return matrix;
 
+	// optimal approach
+	int col0 = 1;
 
+    for(int i=0;i<n;i++){
+
+        for(int j=0;j<m;j++){
+
+            if(matrix[i][j]==0){
+
+                //mark the i-th row
+
+                matrix[i][0] = 0;
+
+                //mark the j-th col
+
+                if(j !=0 ) matrix[0][j] = 0;
+
+                else col0 = 0;
+
+            }
+
+        }
+
+    }
+
+    for(int i=0;i<n;i++){
+
+        for(int j=0;j<m;j++){
+
+            if(matrix[i][j]!=0){
+
+                if(matrix[i][0]==0||matrix[0][j]==0) matrix[i][j]=0;
+
+            }
+
+        }
+
+    }
+
+    if(matrix[0][0]==0){
+
+        for(int j=0;j<m;j++) matrix[0][j] = 0;
+
+    }
+
+    if(col0==0){
+
+        for(int i=0;i<n;i++) matrix[i][0] = 0;
+
+    }
+
+                
+
+    return matrix;
 }

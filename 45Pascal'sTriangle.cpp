9bbda5e18215b97bd980bd701fd2669980036edd
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-09-30 22:35:17 +0530

    Update 45Pascal'sTriangle.cpp

--- 45Pascal'sTriangle.cpp
+++ 45Pascal'sTriangle.cpp
@@ -29,3 +29,34 @@ vector<vector<int>> pascalTriangle(int N) {
     }
     return ans;
 }
+
+// Second Approach
+vector<int> genrateRow(int row){
+
+    long long ans=1;
+    vector<int> ansRow;
+
+    ansRow.push_back(ans);
+
+    for(int col=1;col<row;col++){
+
+        ans=ans*(row-col);
+        ans=ans/col;
+
+        ansRow.push_back(ans);
+    }
+
+    return ansRow;
+
+}
+
+
+vector<vector<int>> pascalTriangle(int N) {
+    
+    vector<vector<int>> ans;
+    for(int i=1;i<=N;i++){
+        ans.push_back(genrateRow(i));
+    }
+
+    return ans;
+}

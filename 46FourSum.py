a12291ce4032396db096053cbde8a67949163cfa
hsumit263 <139766308+hsumit263@users.noreply.github.com>
2023-10-15 00:04:20 +0530

    Update 46FourSum.py

--- 46FourSum.py
+++ 46FourSum.py
@@ -48,7 +48,41 @@ def fourSum(nums: [int], target: int) -> [[int]]:
     # ans=[list(t) for t in st]
     # return ans
 
-   
+    # Optimal approach
+    n=len(nums)
+    ans=[]
+
+    nums.sort()
+
+    for i in range(n):
+        if(i>0 and nums[i]==nums[i-1]): continue
+
+        for j in range(i+1,n):
+            if(j>i+1 and nums[j]==nums[j-1]): continue
+
+            k=j+1
+            l=n-1
+
+            while(k<l):
+
+                sum_=nums[i]+nums[j]+nums[k]+nums[l]
+                if(sum_ == target):
+                    temp=[nums[i],nums[j],nums[k],nums[l]]
+                    ans.append(temp)
+
+                    k+=1
+                    l-=1
+
+                    while(k<l and nums[k]==nums[k-1]): k+=1
+                    while(k<l and nums[l]==nums[l+1]): l-=1
+
+                elif sum_<target:
+                    k+=1
+
+                else: l-=1
+
+    
+    return ans 
 
 
 
